05fb398a2863bae1c7a3ebac308e998ece272e35
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2025-03-08 14:28:49 +0200

    Added the contact and donate page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import KofiWidget from "@/components/KofiWidget";
 import Navbar from "@/components/Navbar/Navbar";
 import { useRouter } from "next/router";
 import { LangProvider } from "@/components/LangContext";
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <LangProvider>
         <Navbar currentPage={router.pathname} />
         <Component {...pageProps} />
+        <KofiWidget />
       </LangProvider>
     </>
   );
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,44 @@
+import LanguageToggle from "@/components/LanguageToggle/LanguageToggle";
+import { useLangContext } from "@/components/LangContext";
+
 export default function Contact() {
-  return <h1>Contact Page</h1>;
+  const { lang, toggleLang } = useLangContext();
+  return (
+    <>
+      <LanguageToggle />
+      {lang === "en" ? (
+        <div className="container">
+          <div>🍻 Get in Touch!</div>
+          <div className="contactparagraph">
+            Found a bug? Got a wild idea for a new feature? Or just want to chat
+            about the game (or drinks)? 🍹🍺 <br />
+            I’d love to hear from you! Shoot me an email by clicking{" "}
+            <a className="underline" href="mailto:24dimitris@gmail.com">
+              here
+            </a>
+            , and I’ll get back to you as soon as I can!
+            <br /> Thanks for playing!
+          </div>
+          <div> 🥂Cheers!</div>
+        </div>
+      ) : (
+        <div className="container">
+          <div>🍻 Ας μιλήσουμε!</div>
+          <div className="contactparagraph">
+            Βρήκες κάποιο bug; Έχεις καποια τρελή ιδέα για νέα λειτουργία; Ή
+            απλά θέλεις να πιάσουμε την κουβέντα για το παιχνίδι (ή τα ποτά);
+            🍹🍺
+            <br />
+            Θα χαρώ να ακούσω νέα σου! Στείλε μου ένα email πατώντας {""}
+            <a className="underline" href="mailto:24dimitris@gmail.com">
+              εδώ
+            </a>
+            , και θα σου απαντήσω το συντομότερο δυνατό!
+            <br /> Ευχαριστώ για την υποστήριξη!!
+          </div>
+          <div> 🥂Για μας!</div>
+        </div>
+      )}
+    </>
+  );
 }
